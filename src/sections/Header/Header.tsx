6d948280f3ef740ed596dea8a07e8a3a5e98802f
b16z <228128378+bee16zee@users.noreply.github.com>
2025-12-13 16:45:37 -0600

    enh: add staker console title

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import React from "react";
 import { useAccount } from "wagmi";
 
 const Header: React.FC = () => {
   const { isConnected } = useAccount();
 
   return (
-    <Box className="flex h-16 items-center justify-end border-b border-divider bg-[#171717] px-3">
+    <Box className="flex h-16 items-center justify-between border-b border-divider bg-[#171717] px-3">
+      <Box className="flex items-center">
+        <Typography variant="h6" className="font-thin">
+          Staker Console
+        </Typography>
+      </Box>
       <Box className="flex items-center gap-4">
         {isConnected && (
           <>

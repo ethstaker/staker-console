5de58926c5685907cd60dc4af5f2ddc618e476b9
Yorick Downe <71337066+yorickdowne@users.noreply.github.com>
2026-09-04 18:52:34 +0100

    Update Vite to 8 (#266)
    * Update Vite to 8
    
    * Chonky boi
    
    * Comment indentation
    
    ---------
    
    Co-authored-by: valefar-on-discord <124839138+valefar-on-discord@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -48,11 +48,13 @@ export default defineConfig(({ command, mode }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
+        '@': path.resolve(import.meta.dirname, './src'),
       },
     },
     build: {
       sourcemap: false,
+      // The app cannot be broken up without major refactor. We accept its size
+      chunkSizeWarningLimit: 1000,
       rollupOptions: {
         output: {
           entryFileNames: 'assets/[name].[hash].js',
@@ -77,7 +79,7 @@ export default defineConfig(({ command, mode }) => {
       },
     },
     test: {
-      root: path.resolve(__dirname, './src'),
+      root: path.resolve(import.meta.dirname, './src'),
     },
   }
 });
